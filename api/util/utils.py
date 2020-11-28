8141487fb8e4cfee399a5a3970d117da5bac8845
Conn_Lost <hi.imhpc@outlook.com>
2020-11-29 05:59:43 +0800

    add zero o clock method

--- api/util/utils.py
+++ api/util/utils.py
@@ -15,11 +15,7 @@ def reply_json(code, msg=None, data=None):
             'msg': REPLY_CODES[code] if msg is None else msg,
             'data': data
         })
-    return jsonify({
-        'code': 400,
-        'msg': 'Unknown code',
-        'data': data
-    })
+    return jsonify({'code': 400, 'msg': 'Unknown code', 'data': data})
 
 
 def captureFace():
@@ -30,47 +26,51 @@ def captureFace():
 
 
 def get_indoor_temp():
-	# import os
-	# ds18b20 = ''
-	# for i in os.listdir('/sys/bus/w1/devices'):
-	# 	if i != 'w1_bus_master1':
-	# 		ds18b20 = i
-	# location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
+    # import os
+    # ds18b20 = ''
+    # for i in os.listdir('/sys/bus/w1/devices'):
+    # 	if i != 'w1_bus_master1':
+    # 		ds18b20 = i
+    # location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
 
-	# tfile = open(location)
-	# text = tfile.read()
-	# tfile.close()
-	# temp_data = float(text.split(" ")[-1][2:])/1000
+    # tfile = open(location)
+    # text = tfile.read()
+    # tfile.close()
+    # temp_data = float(text.split(" ")[-1][2:])/1000
 
-	# if temp_data != None:
-	# 	return '%0.1f' % temp_data
-	# else:
-	# 	return None
+    # if temp_data != None:
+    # 	return '%0.1f' % temp_data
+    # else:
+    # 	return None
 
-	import util.dht11 as dht11
+    import util.dht11 as dht11
 
-	# read data using pin 14
-	instance = dht11.DHT11(pin = 22)
-	result = instance.read()
+    # read data using pin 14
+    instance = dht11.DHT11(pin=22)
+    result = instance.read()
 
-	if result.is_valid():
-		print("Temperature: %-3.1f C" % result.temperature)
-		print("Humidity: %-3.1f %%" % result.humidity)
-		return [result.temperature, result.humidity]
-	else:
-		print("Error: %d" % result.error_code)
-		return False
-
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print(result.humidity)
+        return [result.temperature, result.humidity]
+    else:
+        print("Error: %d" % result.error_code)
+        return False
 
 
 def get_outdoor_weather():
-	ip = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
-	location = requests.get('http://api.ipstack.com/'+ip+'?'+SC['location']).json()
-	lon = str(location.get("longitude"))
-	lat = str(location.get("latitude"))
-	weather = requests.get('http://api.openweathermap.org/data/2.5/weather?'+SC['weather']+'&units=metric&lat='+lat+'&lon='+lon).json()
-	weather['weather'][0]['icon'] = "http://openweathermap.org/img/w/"+weather['weather'][0]['icon']+".png"
-	return weather
+    ip = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
+    location = requests.get('http://api.ipstack.com/' + ip + '?' +
+                            SC['location']).json()
+    lon = str(location.get("longitude"))
+    lat = str(location.get("latitude"))
+    weather = requests.get('http://api.openweathermap.org/data/2.5/weather?' +
+                           SC['weather'] + '&units=metric&lat=' + lat +
+                           '&lon=' + lon).json()
+    weather['weather'][0][
+        'icon'] = "http://openweathermap.org/img/w/" + weather['weather'][0][
+            'icon'] + ".png"
+    return weather
 
 
 def get_current_time():
@@ -80,13 +80,19 @@ def get_current_time():
 def get_time_gap(old):
     return int(time.time()) - old
 
+
 def get_weighted_value(ori):
-	if ori >= 0.9:
-		ori = ori*3/2
-	elif ori >= 0.7:
-		ori = ori*6/5
-	elif ori >= 0.5:
-		ori = ori*5/6
-	else:
-		ori = ori/2
-	return ori
+    if ori >= 0.9:
+        ori = ori * 3 / 2
+    elif ori >= 0.7:
+        ori = ori * 6 / 5
+    elif ori >= 0.5:
+        ori = ori * 5 / 6
+    else:
+        ori = ori / 2
+    return ori
+
+def get_zero_clock():
+    t = time.localtime(time.time())
+    time1 = time.mktime(time.strptime(time.strftime('%Y-%m-%d 00:00:00', t),'%Y-%m-%d %H:%M:%S'))
+    return time1
