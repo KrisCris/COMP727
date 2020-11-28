3b54237034580bf3cadb83d2fe6f1eacf2dd50b7
Conn_Lost <hi.imhpc@outlook.com>
2020-11-29 06:01:05 +0800

    happiness / emotion records

--- api/database/Emotions.py
+++ api/database/Emotions.py
@@ -1,5 +1,8 @@
 from database.db import db
 from util.utils import get_current_time
+import time
+
+from database.Working import Working
 
 class Emotions(db.Model):
     __tablename__ = 'emotions'
@@ -24,3 +27,53 @@ class Emotions(db.Model):
     @staticmethod
     def get_last_timestamp():
         return Emotions.query.order_by(Emotions.id.desc()).first().time
+
+    @staticmethod
+    def get_depress_rate():
+        last_work = Working.isWorking()
+        if last_work is None:
+            return None
+        else:
+            begin_time = last_work.begin_time
+            entire = Emotions.query.filter(Emotions.time>begin_time).count()
+            depress = Emotions.query \
+                    .filter(Emotions.time>begin_time) \
+                    .filter((Emotions.emotion == 'sad') | (Emotions.emotion == 'fear') | (Emotions.emotion == 'angry')).count()
+            print(str(entire)+'\t'+str(depress))            
+            return depress/entire if entire != 0 else 0
+
+    @staticmethod
+    def get_periodic_emotion():
+        current = get_current_time()
+        begin = [current-3600, current-3600*3,current-3600*6,current-3600*12]
+        # map = {
+        #     "angry": 1.00001,
+        #     "fear": 2.00001,
+        #     "sad": 3.00001,
+        #     "neutral": 4.00001,
+        #     "surprise": 5.00001,
+        #     "happy": 6.00001
+        # }
+        map = {
+            "angry": 1,
+            "fear": 2,
+            "sad": 3,
+            "neutral": 4,
+            "surprise": 5,
+            "happy": 6
+        }
+        data = [{},{},{},{}]
+        index = 0
+        for t in begin:
+            res = Emotions.query.filter(Emotions.time>t).all()
+            if res is not None:
+                for r in res:
+                    h = time.localtime(r.time).tm_hour
+                    m = time.localtime(r.time).tm_min
+                    h = str(int(h)) if h>=10 else '0'+str(int(h))
+                    m = str(int(m)) if m>=10 else '0'+str(int(m))
+                    tm = h+":"+m
+                    data[index][tm] = map[r.emotion]
+            index += 1
+        return data
+            
