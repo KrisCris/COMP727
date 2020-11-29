4ca4b094d0d3029eea7a3cf5bafbf3ae125421bc
Conn_Lost <hi.imhpc@outlook.com>
2020-11-29 16:30:04 +0800

    qc code scan

--- mobile/lib/CoverPage.dart
+++ mobile/lib/CoverPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class CoverPage extends StatefulWidget {
   @override
@@ -51,11 +54,22 @@ class CoverState extends State<CoverPage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(builder: (context, snapShot) {
-      Future.delayed(Duration(milliseconds: 2500), () {
-        Navigator.of(context).popAndPushNamed('/main', arguments: ['a','b','c']);
-      });
-      return this.getPage("");
-    });
+    return FutureBuilder(
+        future: scanner.scan(),
+        builder: (context, snapShot) {
+          if(snapShot.hasData){
+            String link = snapShot.data;
+            WebSocketChannel c = new IOWebSocketChannel.connect('ws://$link:5050/mobile');
+            c.sink.add('try to connect!');
+            Future.delayed(Duration(milliseconds: 2500), () {
+              Navigator.of(context)
+                  .popAndPushNamed('/main', arguments: c);
+            });
+          } else {
+
+          }
+
+          return this.getPage("Connecting!");
+        });
   }
 }
